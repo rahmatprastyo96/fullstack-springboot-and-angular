b071404a780233bcef2323492a0aebf01d9a105c
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2021-06-29 22:15:21 +0700

    FE-add list product

--- backend-spring-boot/spring-boot-ecommerce/src/main/java/com/codemathdev/ecommerce/dao/ProductRepository.java
+++ backend-spring-boot/spring-boot-ecommerce/src/main/java/com/codemathdev/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.codemathdev.ecommerce.dao;
 
 import com.codemathdev.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
